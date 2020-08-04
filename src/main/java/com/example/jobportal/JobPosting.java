06b9b281cef2c7d9c64684b006b5b6685fd4c967
Alisha <alishasamal18@gmail.com>
2020-08-04 19:47:04 +0530

    Initial commit

---
+++ src/main/java/com/example/jobportal/JobPosting.java
@@ -0,0 +1,14 @@
+package com.example.jobportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobPosting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_posting);
+    }
+}
